943d42a025b94750ee7bef1ecedbc51fb2a7c65b
Levin <dinglw@live.cn>
2016-08-02 21:11:17 +0800

    add print

--- mysite/views.py
+++ mysite/views.py
@@ -25,7 +25,7 @@ class WeixinInterfaceView(View):
 		tmpList = [token, timestamp, nonce]
 		tmpList.sort()
 		tmpstr = '%s%s%s' % tuple(tmpList)
-                print tmplist
+                print tmpList
                 print echostr		
 		#sha1 encode method
 		tmpstr = hashlib.sha1(tmpstr).hexdigest()
