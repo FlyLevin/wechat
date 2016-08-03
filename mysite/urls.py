51cf9b9af8db7d4a07f16357f3232a2fef78d811
Levin <dinglw@live.cn>
2016-08-03 19:46:15 +0800

    remove some reduntent files

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,5 +16,7 @@ Including another URLconf
 from django.conf.urls import patterns, include, url
 from mysite.views import WeixinInterfaceView
 #from django.contrib import admin
+from django.views.decorators.csrf import csrf_exempt
 
-urlpatterns = patterns('', url(r'^weixin$', WeixinInterfaceView.as_view()),)
+
+urlpatterns = patterns('', url(r'^weixin/', csrf_exempt(WeixinInterfaceView.as_view())), )
