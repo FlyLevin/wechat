c95823a11ed824465a5b6f2ccbcff06fddc96a39
Levin <dinglw@live.cn>
2016-08-03 19:03:48 +0800

    csrf spelling wrong

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import patterns, include, url
 from mysite.views import WeixinInterfaceView
 #from django.contrib import admin
-from django.views.decorators.crsf import csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = patterns('', url(r'^weixin/', csrf_exempt(WeixinInterfaceView.as_view())),)
