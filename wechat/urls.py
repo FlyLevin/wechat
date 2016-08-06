0d8e5d53f842cb2d5e8c4bc79e43018e71dc10bf
Levin <dinglw@live.cn>
2016-08-06 16:09:40 +0800

    remove the mysite and wechat

--- wechat/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-from django.conf.urls import patterns, url
-from django.views.decorators.csrf import csrf_exempt
-from wechat import views
-
-
-urlpatterns = patterns('',
-    url(r'^$', csrf_exempt(views.WechatInterfaceView.as_view())),
-)
