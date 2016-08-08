9cf732a089315fb3a444f25adcb6c2eb0fec8a4f
Levin <dinglw@live.cn>
2016-08-08 20:27:49 +0800

    change the WSGIHandler into get_wsgi_application

--- yimi/wsgi.py
+++ yimi/wsgi.py
@@ -5,12 +5,12 @@ import os
 import sys
 reload(sys)
 
-import django
-django.setup()
+#import django
+#django.setup()
 
 sys.setdefaultencoding('utf-8')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "yimi.settings")
 
-from django.core.handlers.wsgi import WSGIHandler
-application = WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
