43484df462c4cced817a8fe1ae8db2edfc84d040
Levin <dinglw@live.cn>
2016-08-08 19:27:36 +0800

    change the models16 and self add the first function

--- models16/models16.py
+++ models16/models16.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+class QuerySet(models.QuerySet):
+    def first(self):
+        """
+        Returns the first object of a query, returns None if no match is found.
+        """
+        objects = list((self if self.ordered else self.order_by('pk'))[:1])
+        if objects:
+            return objects[0]
+        return None
 
+    def last(self):
+        """
+        Returns the last object of a query, returns None if no match is found.
+        """
+        objects = list((self.reverse() if self.ordered else self.order_by('-pk'))[:1])
+        if objects:
+            return objects[0]
+        return None
