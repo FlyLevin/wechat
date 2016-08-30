ececc6d8910bf5c1fde5ac2ed33c3ada8dfea96b
Levin <dinglw@live.cn>
2016-08-30 12:41:36 +0800

    add the debug mode in zxxFile.js

--- static/zxxFile.js
+++ static/zxxFile.js
@@ -39,11 +39,13 @@ var ZXXFILE = {
 		// 获取文件列表对象
 		var files = e.target.files || e.dataTransfer.files;
 		//继续添加文件
-                if (this.fileFilter.length > self.MAXFILENUM){
-                        alert('只允许提交三张图片')
+		if (this.fileFilter.length > self.MAXFILENUM){
+			alert('只允许提交三张图片')
                 }
                 else{
 			alert(this.fileFilter.length)
+			alert(this.MAXFILENUM)
+			alert(this.fileFilter.length > self.MAXFILENUM)
 			this.fileFilter = this.fileFilter.concat(this.filter(files));
                 }
 		this.funDealFiles();
