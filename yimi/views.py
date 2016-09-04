c0875ce891009485509d38f88680a17fca8f22f1
Levin <dinglw@live.cn>
2016-09-04 20:57:13 +0800

    add the debug in syncdb

--- yimi/views.py
+++ yimi/views.py
@@ -11,7 +11,7 @@ def syncdb(request):
     sys.stdout = log_out   
     #利用django提供的命令行工具来执行“manage.py syncdb”  
     from django.core.management import execute_from_command_line  
-    execute_from_command_line(["manage.py", "syncdb", "--noinput"])  
+    execute_from_command_line(["manage.py", "syncdb", "--noinput", "--v 3"])  
     #获得“manage.py syncdb”的执行输出结果，并展示在页面  
     result = log_out.getvalue()  
     sys.stdout = saveout  
