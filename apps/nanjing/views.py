df90acc6de43331e0e664105d1cdde69454a3f9c
Levin <dinglw@live.cn>
2016-09-14 11:51:08 +0800

    fix a spelling error in nanjing views

--- apps/nanjing/views.py
+++ apps/nanjing/views.py
@@ -238,7 +238,7 @@ def proposal_add(request, slug):
             return HttpResponseRedirect(reverse_url(slug))
     elif request.method == "GET":
         open_id = request.GET.get('open_id')
-        if openid:
+        if open_id:
             openaccount = appitem.openaccount_set.get(openid=open_id)
             if openaccount:
                 context = {
